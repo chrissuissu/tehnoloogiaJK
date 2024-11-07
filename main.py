4168223bc8e10bc5a796f9d93c7a9a6cfd614705
chrissuissu <160324348+chrissuissu@users.noreply.github.com>
2024-11-07 17:37:55 +0200

    main.py

--- main.py
+++ main.py
@@ -1,34 +1,41 @@
 from settings import *
-from support import *
-from timer import Timer
+from player import Player
 
 class Game:
     def __init__(self):
+        # setup
         pygame.init()
         self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-        pygame.display.set_caption('Monster Battle')
+        pygame.display.set_caption('Survivor')
         self.clock = pygame.time.Clock()
         self.running = True
 
         # groups 
         self.all_sprites = pygame.sprite.Group()
 
+        # sprites
+        self.player = Player((400,300), self.all_sprites)
+
     def run(self):
         while self.running:
+            # dt 
             dt = self.clock.tick() / 1000
+
+            # event loop 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False
-           
-            # update
+
+            # update 
             self.all_sprites.update(dt)
 
-            # draw  
+            # draw
+            self.display_surface.fill('black')
             self.all_sprites.draw(self.display_surface)
             pygame.display.update()
-        
+
         pygame.quit()
-    
+
 if __name__ == '__main__':
     game = Game()
     game.run()
